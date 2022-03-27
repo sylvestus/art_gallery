94f5cce8f21a5b428a77df582f01317eced3c8ef
sylvestus <sylvestus.sigei@student.moringaschool.com>
2022-03-27 16:50:46 +0300

    add our app urls

---
+++ gallery/urls.py
@@ -0,0 +1,16 @@
+from unicodedata import name
+from django.urls import path,re_path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns=[
+    path("",views.gen_landing, name='generalpics'),
+    path('search/picperlocation/',views.pic_per_location,name='picperlocation'),
+    re_path(r'singlepicdetails/(\d+)',views.single_pic_details,name='singlepicdetails'),
+    path('searched/',views.searched,name='searched')
+
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
