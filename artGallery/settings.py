4cd4d5de16934e22f431a4f0e2776626c7e48bf9
sylvestus <sylvestus.sigei@student.moringaschool.com>
2022-03-28 00:11:14 +0300

    up

--- artGallery/settings.py
+++ artGallery/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from distutils.debug import DEBUG
 import os
 from pathlib import Path
 import django_heroku
@@ -29,6 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
+# DEBUG=True
 ALLOWED_HOSTS = []
 
 
